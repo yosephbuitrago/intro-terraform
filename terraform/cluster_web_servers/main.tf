44143df168186ceec4b9a9299dfc0d7170296953
ybuitrago <yoseph.buitrago@rapidratings.com>
2021-04-05 18:36:36 +0100

    add missing vpc id in sg

--- terraform/cluster_web_servers/main.tf
+++ terraform/cluster_web_servers/main.tf
@@ -67,7 +67,7 @@ data "aws_ami" "ami" {
 
 resource "aws_security_group" "alb" {
   name = "${var.basename}-${var.name}-sg"
-
+  vpc_id      = var.network_info.vpc_id
   # Allow inbound HTTP requests
   ingress {
     from_port   = 80
